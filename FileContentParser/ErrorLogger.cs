77f03c923a0167bf9af0f314912f96a6d5e88d01
acharayaP03 <acharyaP03@gmail.com>
2024-11-27 15:03:11 +1100

    preserving eroor logs

---
+++ FileContentParser/ErrorLogger.cs
@@ -0,0 +1,25 @@
+﻿namespace FileContentParser;
+
+public class ErrorLogger
+{
+    private readonly string _logFileName;
+
+    public ErrorLogger(string logFileName)
+    {
+        _logFileName = logFileName;
+    }
+
+
+    public void Log(Exception ex)
+    {
+
+        var fileEntry =
+$@"[{DateTime.Now}]
+Exception message: {ex.Message}
+Stack trace: {ex.StackTrace}
+
+
+";
+        File.AppendAllText(_logFileName, fileEntry);
+    }
+}
--- FileContentParser/Program.cs
+++ FileContentParser/Program.cs
@@ -1,70 +1,91 @@
 ﻿
-
-using Microsoft.VisualBasic;
+using FileContentParser;
 using System.Text.Json;
+FileDataParserApp app = new();
+var errorLogger = new ErrorLogger("errorLog.txt");
 
-var isFileRead = false;
-//string contents = default; // in the case if we do not know the default type,
-//we can use default. if the type is defined before variable is initialized, just assign defalut or else below
-var contents = default(string);
-var fileName = default(string);
-do
-{
-    try
-    {
-        UserConsoleInteraction.PrintApplicationStartingLabel(
-        "File formatter",
-        "Type in your file location to format file."
-        );
-
-        fileName = Console.ReadLine();
-        contents = File.ReadAllText(fileName);
-        isFileRead = true;
-
-    }
-    catch (ArgumentNullException ex)
-    {
-        Console.WriteLine("Sorry!, Filename cannot be empty. please provide file name to be formatted. file name is null.");
-    }
-    catch (ArgumentException ex)
-    {
-        Console.WriteLine("Sorry!, Filename cannot be empty. please provide file name to be formatted.");
-    }
-    catch (FileNotFoundException ex)
-    {
-        Console.WriteLine("Sorry!, Filename could not be found..");
-    }
-} while (!isFileRead);
-
-List<FileContents> formatedContents = default;
 try
 {
-    formatedContents = JsonSerializer.Deserialize<List<FileContents>>(contents);
 
-} catch(JsonException ex)
+app.RunApp();
+}
+catch(Exception ex)
 {
-    var originalColor = Console.ForegroundColor;
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine($"JSON on {fileName} file was not "
-        + $"in a valid format. JSON body.");
-    Console.WriteLine(contents);
-    Console.ForegroundColor = originalColor;
+    Console.WriteLine($"Sorry, The application has experienced an unexpected error " +
+        "and will have to be closed.");
 
-    throw new JsonException($"{ex.Message}, The file that caused this issue is: {fileName}", ex);
+    errorLogger.Log(ex);
 }
 
-if (formatedContents.Count > 0)
-{
-    Console.WriteLine();
-    Console.WriteLine("File read contents are:");
-    foreach (var fileContent in formatedContents)
-    {
-        Console.WriteLine(fileContent);
-    }
-}
 
 Console.ReadKey();
 
+public class FileDataParserApp
+{
+    public void RunApp()
+    {
+        var isFileRead = false;
+        //string contents = default; // in the case if we do not know the default type,
+        //we can use default. if the type is defined before variable is initialized, just assign defalut or else below
+        var contents = default(string);
+        var fileName = default(string);
+        do
+        {
+            try
+            {
+                UserConsoleInteraction.PrintApplicationStartingLabel(
+                "File formatter",
+                "Type in your file location to format file."
+                );
+
+                fileName = Console.ReadLine();
+                contents = File.ReadAllText(fileName);
+                isFileRead = true;
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Sorry!, Filename cannot be empty. please provide file name to be formatted. file name is null.");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Sorry!, Filename cannot be empty. please provide file name to be formatted.");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Sorry!, Filename could not be found..");
+            }
+        } while (!isFileRead);
+
+        List<FileContents> formatedContents = default;
+        try
+        {
+            formatedContents = JsonSerializer.Deserialize<List<FileContents>>(contents);
+
+        }
+        catch (JsonException ex)
+        {
+            var originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"JSON on {fileName} file was not in a valid format. JSON body.");
+            Console.WriteLine(contents);
+
+            Console.ForegroundColor = originalColor;
+
+            throw new JsonException($"{ex.Message}, The file that caused this issue is: {fileName}", ex);
+        }
+
+        if (formatedContents.Count > 0)
+        {
+            Console.WriteLine();
+            Console.WriteLine("File read contents are:");
+            foreach (var fileContent in formatedContents)
+            {
+                Console.WriteLine(fileContent);
+            }
+        }
+    }
+}
 
 public class UserConsoleInteraction
 {
