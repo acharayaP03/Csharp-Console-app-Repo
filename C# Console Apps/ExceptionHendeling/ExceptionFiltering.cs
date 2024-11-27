90cc3e05661957be66b05af49900e40d90a8c3a3
acharayaP03 <acharyaP03@gmail.com>
2024-11-27 13:31:08 +1100

    initial file formatter project creation

---
+++ C# Console Apps/ExceptionHendeling/ExceptionFiltering.cs
@@ -0,0 +1,9 @@
+﻿namespace C__Console_Apps.ExceptionHendeling;
+
+public static class ExceptionFiltering
+{
+    public static IEnumerable<string> SendHttpRequest(string requestUrl)
+    {
+        return ["test", "test1"];
+    }
+}
---
+++ FileContentParser/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+UserConsoleInteraction.PrintApplicationStartingLabel(
+    "File formatter",
+    "Type in your file location to format file."
+    );
+
+var userInput = Console.ReadLine();
+
+Console.WriteLine("User input " + userInput );
+
+Console.ReadKey();
+
+
+public class UserConsoleInteraction
+{
+
+
+    public static void PrintApplicationStartingLabel(string applicationLabel, string applicationSubtitle)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine($"************* Welcome to {applicationLabel} *************");
+        Console.ResetColor();
+        Console.WriteLine($"{applicationSubtitle}");
+
+    }
+}
